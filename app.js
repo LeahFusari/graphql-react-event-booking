5faf69f5f55f3e9ad7be48023473b119beb5b812
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2022-03-03 13:59:07 -0500

    video 4 complete

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+//temp before database creation
+const events =[]
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World');
-})
+app.use('/graphql', graphqlHTTP({
+    // the first ! is so it cant return an array of null values and the second ! is so that the field itself cannot be null
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
 
-app.listen(3000);
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery{
+            events: [Event!]! 
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return events
+        },
+        createEvent: (args) => {
+           const event = {
+               _id: Math.random().toString(), //temp until database is created to generate one
+               title: args.eventInput.title,
+               description: args.eventInput.description,
+               price: +args.eventInput.price, //+ makes sure the price arg is converted to a float 
+               date: args.eventInput.date
+           };
+           events.push(event);
+           return event;
+        }
+    },
+    graphiql: true
+})
+);
+
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
